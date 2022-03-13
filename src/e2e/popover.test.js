da69474076142a8e7dec42fefa75eb510d67656e
Dmitry Vinogradov <mr.dmitryvinogradov@gmail.com>
2022-03-13 14:54:54 +0300

    Linter fix

--- src/e2e/popover.test.js
+++ src/e2e/popover.test.js
@@ -2,7 +2,7 @@ import puppeteer from 'puppeteer';
 
 jest.setTimeout(15000);
 describe('popover', () => {
-  const APP = 'http://localhost:9000/'
+  const APP = 'http://localhost:9000/';
   let page = null;
   let browser = null;
   const width = 1920;
@@ -17,10 +17,10 @@ describe('popover', () => {
     page = await browser.newPage();
     await page.setViewport({ width, height });
   });
-    test('should show popover on click', async () => {
-      await page.goto(APP);
-      const btn = await page.$('.btn')
-      btn.click();
-      await page.waitForSelector('.popover');
-    }, 10000)
+  test('should show popover on click', async () => {
+    await page.goto(APP);
+    const btn = await page.$('.btn');
+    btn.click();
+    await page.waitForSelector('.popover');
+  }, 10000);
 });
