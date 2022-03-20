b3c8e1895a64768fd9b123d0c280bb216e4093f9
Dmitry Vinogradov <mr.dmitryvinogradov@gmail.com>
2022-03-20 16:33:22 +0300

    e2e problem

--- e2e/e2e.server.js
+++ e2e/e2e.server.js
@@ -1,8 +1,8 @@
 const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
+const DevServer = require('webpack-dev-server');
 const config = require('../webpack.dev');
 
-const server = new WebpackDevServer(webpack(config), {});
+const server = new DevServer(webpack(config), {});
 server.listen(8080, 'localhost', (err) => {
   if (err) {
     return;
